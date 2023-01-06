77df4db30324aa8d3acb2d4d2a20cbcd7fbaf33a
georgievmv <96878439+georgievmv@users.noreply.github.com>
2023-01-06 16:22:18 +0200

    slider update

--- src/components/AddEvent.tsx
+++ src/components/AddEvent.tsx
@@ -3,27 +3,26 @@ import { Form, Button } from "react-bootstrap";
 import { useState, useContext, useEffect } from "react";
 import { Context } from "../store/app-context";
 import useFireStore from "../hooks/useFireStore";
+import Slider from "rc-slider";
+import Range from "rc-slider";
+import "rc-slider/assets/index.css";
 
 const AddEvent = () => {
   const [startTime, setStartTime] = useState(720);
-  const [endTime, setEndTime] = useState(720);
+  const [endTime, setEndTime] = useState(800);
   const [eventTitle, setEventTitle] = useState("");
   const firestore = useFireStore();
   const ctx = useContext(Context);
 
-  const startRangeHandler = (e: React.FormEvent<HTMLInputElement>) => {
-    const newTime = parseInt(e.currentTarget.value);
-    setStartTime(newTime);
-  };
-  const endRangeHandler = (e: React.FormEvent<HTMLInputElement>) => {
-    const newTime = parseInt(e.currentTarget.value);
-    setEndTime(newTime);
+  const sliderChangeHandler = (value: any) => {
+    setStartTime(value[0]);
+    setEndTime(value[1]);
   };
 
   const timeTransformer = (time: number) => {
     let hour = time / 60;
     let minutes = ":30";
-    let roundedHour = Math.round(hour).toString();
+    let roundedHour = Math.floor(hour).toString();
     if (time % 60 == 0) {
       minutes = ":00";
     }
@@ -66,33 +65,65 @@ const AddEvent = () => {
         <Form.Group>
           <Form.Control type="date"></Form.Control>
         </Form.Group>
-        <Form.Group>
-          <Form.Label>Starting Time:</Form.Label>
-          <h5>{timeTransformer(startTime)}</h5>
-          <Form.Range
-            onChange={startRangeHandler}
-            value={startTime}
-            step={30}
-            max={1440}
-          ></Form.Range>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Ending Time</Form.Label>
-          <h5>{timeTransformer(endTime)}</h5>
-          <Form.Range
-            onChange={endRangeHandler}
-            value={endTime}
-            step={30}
-            max={1440}
-          ></Form.Range>
-        </Form.Group>
-        <Button
-          disabled={endTime <= startTime || !eventTitle}
-          type="submit"
-          variant="success"
+        <div
+          style={{
+            marginTop: "2rem",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
         >
-          Add
-        </Button>
+          <Form.Group>
+            <Form.Label>Starting Time:</Form.Label>
+            <h5>{timeTransformer(startTime)}</h5>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Ending Time</Form.Label>
+            <h5>{timeTransformer(endTime)}</h5>
+          </Form.Group>
+        </div>
+        <Slider
+          onChange={sliderChangeHandler}
+          railStyle={{ backgroundColor: "#30115e", height: "7px" }}
+          handleStyle={[
+            {
+              cursor: "pointer",
+              border: "none",
+              backgroundColor: "#ea39b8",
+              opacity: "1",
+              height: "16px",
+              width: "16px",
+            },
+            {
+              cursor: "ponter",
+              border: "none",
+              backgroundColor: "#ea39b8",
+              opacity: "1",
+              height: "16px",
+              width: "16px",
+            },
+          ]}
+          trackStyle={[
+            { backgroundColor: "#30115e" },
+            { backgroundColor: "#30115e" },
+          ]}
+          range
+          allowCross={false}
+          defaultValue={[720, 800]}
+          min={0}
+          max={1440}
+          step={30}
+        />
+        <div style={{ textAlign: "center" }}>
+          <Button
+            className="mt-4"
+            disabled={endTime <= startTime || !eventTitle}
+            type="submit"
+            variant="success"
+          >
+            Add
+          </Button>
+        </div>
       </Form>
     </div>
   );
